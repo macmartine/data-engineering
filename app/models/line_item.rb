b09859e8db61c4179dc0d9ed85d21c276cd504fb
Mac Martine <public@macmartine.com>
2012-10-31 13:35:59 -0700

    of course don't change the tabs in teh tab file to spaces

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,6 @@
 class LineItem < ActiveRecord::Base
   attr_accessible :item_id, :purchase_count, :purchaser_id, :item
-   has_one :merchant
-   belongs_to :purchaser
-   belongs_to :item
+  has_one :merchant
+  belongs_to :purchaser
+  belongs_to :item
 end
